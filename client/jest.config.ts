3a8d67a7732e50e95f90e36021126bd57e9bbad8
Dennie <oculareo@gmail.com>
2025-04-18 19:44:28 -0400

    update tsconfig, package.json

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -10,6 +10,14 @@ const config: Config = {
   },
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
 
 export default config;
