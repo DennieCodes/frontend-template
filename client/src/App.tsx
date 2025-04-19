26bd546387b920989f33031ac7c5b3e9080c0b36
Dennie <oculareo@gmail.com>
2025-04-18 22:09:52 -0400

    installed fluentui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+
+import { Stack } from "@fluentui/react"
+
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Senior Living Services</h1>
-    </>
+    <BrowserRouter>
+      <Stack>
+        <h1>Senior Living Services</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Stack>
+    </BrowserRouter>
   );
 }
 
